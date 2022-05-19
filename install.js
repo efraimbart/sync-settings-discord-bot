6c8daadef54f98195e5f8f2eb5a5ada12b140eac
efraimbart <efraimbart@gmail.com>
2022-05-19 16:53:04 -0400

    revert

--- install.js
+++ install.js
@@ -1,11 +1,11 @@
-// import { DiscordRequest } from './utils.js';
+import { DiscordRequest } from './utils.js';
 import { SETTING_COMMAND } from './commands.js';
   
 // API endpoint to get and post guild commands
 const endpoint = `applications/${process.env.APP_ID}/commands`;
 // install command
 try {
-    // await DiscordRequest(endpoint, { method: 'POST', body: command });
+    await DiscordRequest(endpoint, { method: 'POST', body: command });
 } catch (err) {
     console.error(err);
 }
