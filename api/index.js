9bf0f20b801e41c4889cf779c11a8b38c2c359d2
efraimbart <efraimbart@gmail.com>
2022-05-19 17:14:21 -0400

    test fix

--- api/index.js
+++ api/index.js
@@ -71,4 +71,9 @@ app.post('/api/interactions', async function (req, res) {
   }
 });
 
-module.exports = app;
+// Get port, or default to 3000
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Listening on port', PORT);
+});
