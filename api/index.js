7b606b057d98445d03881e5180f2911ad058e792
efraimbart <efraimbart@gmail.com>
2022-05-19 21:04:36 -0400

    test further

--- api/index.js
+++ api/index.js
@@ -74,7 +74,7 @@ app.post('/api/interactions', async function (req, res) {
   }
 });
 
-app.get('/setting', async function (req, res) {
+app.get('/api', async function (req, res) {
   return res.redirect(301, `sync-settings://${req.params.setting}`)
 })
 
